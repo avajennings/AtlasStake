8f99b9a9e388928ad8d7337ab2046aec57a1d057
avajennings <rsmiitzgr5102@hotmail.com>
2025-12-25 11:12:22 +0800

    add AtlasStake deployment script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedAtlasStake = await deploy("AtlasStake", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`AtlasStake contract: `, deployedAtlasStake.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_atlasStake"; // id required to prevent reexecution
+func.tags = ["AtlasStake"];
